e0cff69fc9c4814cb58556584b01039b01f4b394
Kelvin Ochubili <kelvinochubili@gmail.com>
2025-01-02 12:59:32 +0100

    feat: add search and filter components, enhance tab layout with featured and recommendation cards

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,14 +1,74 @@
-import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Card, FeaturedCard } from '@/components/Cards';
+
+import icons from '@/constants/icons';
+import images from '@/constants/images';
+import Search from '@/components/Search';
+import Filters from '@/components/Filters';
 
 export default function Index() {
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}>
-			<Text className='my-10 font-rubik text-3xl'>Welcome to Restate</Text>
-		</View>
+		<SafeAreaView className='bg-white h-full'>
+			<ScrollView>
+				<View className='px-5'>
+					<View className='flex flex-row items-center justify-between mt-5'>
+						<View className='flex flex-row items-center'>
+							<Image source={images.avatar} className='size-12 rounded-full' />
+							<View className='flex flex-col items-start ml-2 justify-center'>
+								<Text className='text-xs font-rubik text-black-100'>
+									Good Morning
+								</Text>
+								<Text className='text-base font-rubik-medium text-black-300'>
+									Kelvin
+								</Text>
+							</View>
+						</View>
+
+						<Image source={icons.bell} className='size-6' />
+					</View>
+
+					<Search />
+
+					<View className='my-5'>
+						<View className='flex flex-row items-center justify-between'>
+							<Text className='text-xl font-rubik-bold text-black-300'>
+								Featured
+							</Text>
+							<TouchableOpacity>
+								<Text className='text-base font-rubik-bold text-primary-300'>
+									See All
+								</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
+
+					<ScrollView horizontal showsHorizontalScrollIndicator={false}>
+						<View className='flex flex-row gap-5 mt-5'>
+							<FeaturedCard />
+							<FeaturedCard />
+						</View>
+					</ScrollView>
+
+					<View className='my-5'>
+						<View className='flex flex-row items-center justify-between'>
+							<Text className='text-xl font-rubik-bold text-black-300'>
+								Our Recommendations
+							</Text>
+							<TouchableOpacity>
+								<Text className='text-base font-rubik-bold text-primary-300'>
+									See All
+								</Text>
+							</TouchableOpacity>
+						</View>
+						<Filters />
+					</View>
+					<View className='flex flex-row gap-5 mt-5'>
+						<Card />
+						<Card />
+					</View>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	);
 }
---
+++ components/Search.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { router, useLocalSearchParams, usePathname } from 'expo-router';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import { useDebouncedCallback } from 'use-debounce';
+
+import icons from '@/constants/icons';
+
+const Search = () => {
+	const path = usePathname();
+	const params = useLocalSearchParams<{ query?: string }>();
+	const [search, setSearch] = useState(params.query);
+
+	const debouncedSearch = useDebouncedCallback(
+		(value: string) => router.setParams({ query: value }),
+		500
+	);
+
+	const handleSearch = (value: string) => {
+		setSearch(value);
+	};
+
+	return (
+		<View className='flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2'>
+			<View className='flex-1 flex flex-row items-center justify-start z-50'>
+				<Image source={icons.search} className='size-5' />
+				<TextInput
+					value={search}
+					onChangeText={handleSearch}
+					placeholder='Search for anything'
+					className='text-sm font-rubik text-black-300 ml-2'
+					placeholderTextColor='#666876'
+				/>
+			</View>
+
+			<TouchableOpacity>
+				<Image source={icons.filter} className='size-5' />
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+export default Search;
